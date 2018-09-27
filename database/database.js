a4a3e6dee8fa9c2db79fcd628520780d695b3b48
Sarah Silva <sarahwithanhwilson@gmail.com>
2018-09-27 14:53:21 -0700

    add database env

--- database/database.js
+++ database/database.js
@@ -1,7 +1,13 @@
 const Sequelize = require('sequelize');
+// const orm = new Sequelize('HomeChef', 'root', '', {
+//   dialect: 'mysql',
+// });
 
-const orm = new Sequelize('HomeChef', 'root', '', {
-  dialect: 'mysql',
+const orm = new Sequelize({
+  host: `${process.env.DB_HOST}`,
+  user: `${process.env.DB_USER}`,
+  password: `${process.env.DB_PASS}`,
+  database: `${process.env.DB_DATABASE}`,
 });
 
 orm
