5f5f41e23399999f6b9612207a9d881b98cf3d5f
Duke Goulden <dukegoulden@gmail.com>
2018-09-28 13:42:59 -0500

    change to user/chef

---
+++ src/app/routes.js
@@ -0,0 +1,12 @@
+import ChefAuth from './auth/ChefAuth';
+import UserAuth from './auth/UserAuth';
+import ChefHome from './chefs/ChefHome';
+import UserHome from './users/UsersHome';
+
+export default [
+  { path: '/', component: UserAuth, exact: true },
+  { path: '/auth/user', component: UserAuth, exact: true },
+  { path: '/auth/chef', component: ChefAuth, exact: true },
+  { path: '/chef', component: ChefHome, exact: true },
+  { path: '/user', component: UserHome, exact: true }
+];
---
+++ src/app/users/UsersHome.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const UserHome = () => <div>User Home View Here</div>;
+
+export default UserHome;
