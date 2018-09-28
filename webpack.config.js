26055a15f1c4428000a13d30c736f7c893e70893
Stephen Zerfas <stephen.zerfas@gmail.com>
2018-09-28 10:15:48 -0700

    Store webpack configuration so can revert back and share react router with team

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,13 @@ const SRC_DIR = path.join(__dirname, './src');
 const DIST_DIR = path.join(__dirname, './dist');
 
 module.exports = {
-  entry: `${SRC_DIR}/index.jsx`,
+  entry: {
+    main: `${SRC_DIR}/index.jsx`,
+  },
   output: {
     path: DIST_DIR,
-    filename: 'main.js',
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
   },
   module: {
     rules: [
