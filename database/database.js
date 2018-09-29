e1963d85a2ed74f689aec8f26c5cdfe668393a21
Sarah Silva <sarahwithanhwilson@gmail.com>
2018-09-28 17:13:29 -0700

    add working database

--- database/database.js
+++ database/database.js
@@ -1,13 +1,19 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 // const orm = new Sequelize('HomeChef', 'root', '', {
 //   dialect: 'mysql',
 // });
 
-const orm = new Sequelize({
+// const orm = new Sequelize({
+//   host: `${process.env.DB_HOST}`,
+//   user: `${process.env.DB_USER}`,
+//   password: `${process.env.DB_PASS}`,
+//   database: `${process.env.DB_DATABASE}`,
+// });
+
+const orm = new Sequelize(`${process.env.DB_DATABASE}`, `${process.env.DB_USER}`, `${process.env.DB_PASS}`, {
   host: `${process.env.DB_HOST}`,
-  user: `${process.env.DB_USER}`,
-  password: `${process.env.DB_PASS}`,
-  database: `${process.env.DB_DATABASE}`,
+  dialect: 'mysql',
 });
 
 orm
@@ -35,10 +41,10 @@ const Chef = orm.define('chef', {
 });
 
 const Event = orm.define('event', {
-  // foreign key auto created: chefId
   date: Sequelize.DATEONLY,
   startTime: Sequelize.STRING,
   endTime: Sequelize.STRING,
+  // foreign key auto created: chefId
 });
 
 const ItemEvent = orm.define('itemEvent', {
@@ -82,9 +88,9 @@ const User = orm.define('user', {
   // payment_info: ?
 });
 
-/* //////////////////////////// */
-/* RELATIONSHIPS (alphabetized) */
-/* //////////////////////////// */
+// /* //////////////////////////// */
+// /* RELATIONSHIPS (alphabetized) */
+// /* //////////////////////////// */
 
 Chef.hasMany(Event);
 Chef.hasMany(MenuItem);
@@ -117,4 +123,24 @@ User.hasMany(Order);
 User.hasMany(Rating);
 User.hasMany(Transaction);
 
+// /* ///////////// */
+// /* Create Tables */
+// /* ///////////// */
+User.sync();
+Chef.sync();
+Rating.sync();
+MenuItem.sync();
+Event.sync();
+Transaction.sync();
+ItemEvent.sync();
+Order.sync();
+
 exports.connection = orm;
+exports.Chef = Chef;
+exports.Event = Event;
+exports.ItemEvent = ItemEvent;
+exports.MenuItem = MenuItem;
+exports.Order = Order;
+exports.Rating = Rating;
+exports.Transaction = Transaction;
+exports.User = User;
--- server/server.js
+++ server/server.js
@@ -2,11 +2,18 @@ const express = require('express');
 // const bodyparser = require('body-parser');
 
 const app = express();
-// const db = require('./../database/database.js');
+const db = require('./../database/database.js');
 
 const port = process.env.PORT || 5678;
 
 app.use(express.static(`${__dirname}/../dist`));
+
+// Test Database:
+app.post('/test', (req, res) => {
+  db.Chef.create({ name: 'sarah silva', username: 'chefsarah' });
+  console.log('creating chef');
+  res.end();
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
