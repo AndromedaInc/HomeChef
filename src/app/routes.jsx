40146302ade0e0c27c75361355b9480771b4dd04
Stephen Zerfas <stephen.zerfas@gmail.com>
2018-10-01 12:36:50 -0700

    Improve comments in route.jsx documenting why have taken codesplitting routes have taken so far

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -8,10 +8,11 @@ import UserAuth from './auth/UserAuth';
 // import UsersChefDetails from "./users/UsersChefDetails";
 import AsyncRoute from './AsyncRoute';
 
-// code to handle require.ensure error that would otherwise pop up on server (babel-plugin-dynamic-import-node not working)
+/***** code to handle require.ensure error that would otherwise pop up on server (also tried babel-plugin-dynamic-import-node but could not get to work) *****/
 if (typeof require.ensure !== 'function') require.ensure = (d, c) => { c(require); };
 if (typeof require.include !== 'function') require.include = () => { };
 
+/***** attempt to code split using react-loadable but commented out for now in exchange for AsynRoute.jsx approach detailed by Brian Holt in Frontend Master *****/
 // import Loadable from 'react-loadable';
 // const UserAuth = () => Loadable({
 //     loader: () => import('./auth/UserAuth'),
@@ -38,9 +39,11 @@ if (typeof require.include !== 'function') require.include = () => { };
 //   { path: '/user', component: UserHome(), exact: true },
 // ];
 
+/***** Attempt to create codesplitter helper function but cannot get to work and don't know why since the same logic works without a helper function *****/
 // const codeSplitter = pathToModule => props => <AsyncRoute props={props} loadingPromise={import(pathToModule)} />
 // const ChefAuth = codeSplitter('./auth/ChefAuth')
 
+/***** Codesplitting using AsyncRoute.jsx approach detailed by Brian Holt in Frontend Masters *****/
 const ChefAuth = props => <AsyncRoute props={props} loadingPromise={import('./auth/ChefAuth')} />;
 // console.log('UserHome import() call within routes.jsx is', import('./users/UserHome').then(data => console.log('promise resolved with', data)));
 // const UserAuth = (props) => <AsyncRoute props={props} loadingPromise={import('./auth/UserAuth')} />
