a5fc74ed21dc14f8b3ae651e1dd5228329b569fa
Stephen Zerfas <stephen.zerfas@gmail.com>
2018-10-05 19:25:13 -0700

    Update comment about current error in ChefHome file to prevent future confusion

--- src/app/chefs/ChefHome.jsx
+++ src/app/chefs/ChefHome.jsx
@@ -39,7 +39,7 @@ class ChefHome extends Component {
     this.getAccountInfo();
   }
 
-  // ? Need a component willUnmountMethod that cancels asynchronous tasks? Received following error: Warning: Can't call setState (or forceUpdate) on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in the componentWillUnmount method.
+  // ? Note: We may need a component willUnmountMethod that cancels asynchronous tasks if we decide to keep our current redirect approach if the component does not receive and chefId. As of now, we redirect successfully but receive the following error when attempting to navigate directly to the /chef address with a valid cookie (but since navigating directly, we do not pass in chefId in props) Received following error: "Warning: Can't call setState (or forceUpdate) on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in the componentWillUnmount method."
 
   onChange(e) {
     this.setState({
