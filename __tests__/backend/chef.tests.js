a88418304410a8d3fb58bc7b757d9a9147e894e2
Stephen Zerfas <stephen.zerfas@gmail.com>
2018-10-06 17:25:14 -0700

    Solve bug with pulling in seed file to prepare for more Jest API testing

--- __tests__/backend/chef.tests.js
+++ __tests__/backend/chef.tests.js
@@ -1,5 +1,17 @@
+process.env.NODE_ENV = 'test';
 const request = require('supertest');
-const app = require('../../server/server');
+const fs = require('fs');
+
+// const jest = require('jest');
+
+// jest.mock('../src/app/app');
+const app = require('/Users/Stephan/Desktop/Hack-Reactor/HomeChef/server/server');
+
+console.log(__dirname);
+// const seed = fs.readFileSync('/Users/Stephan/Desktop/Hack-Reactor/HomeChef/database/seedManual.txt');
+const seed = fs.readFileSync(`${__dirname}/../../database/seedManual.txt`);
+
+console.log(seed);
 
 // Super basic test to ensure the test suite is working
 describe('GET /', () => {
